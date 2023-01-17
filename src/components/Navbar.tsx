461a7873f4fc95f002242c66ddb1129e6856c4a2
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-18 00:04:38 +0700

    feat: added dropdown menu to profile and login page in navbar profile picture

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import { IoIosArrowDown } from "react-icons/io";
 
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div className="navbar px-2 bg-gradient-to-b from-bgpurple to-bgpink md:px-5">
@@ -16,13 +18,27 @@ const Navbar = () => {
         <p className="text-white font-medium text-sm hidden md:flex">
           John Doe
         </p>
-        <img
-          className="w-12 rounded-full mx-2"
-          src="src/assets/photo1.jpg"
-          alt="photo"
-        />
-        <div className="text-white">
-          <IoIosArrowDown />
+
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="text-white flex items-center">
+            <img
+              className="w-12 rounded-full mx-2"
+              src="src/assets/photo1.jpg"
+              alt="photo"
+            />
+            <IoIosArrowDown />
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu my-5 p-2 shadow border-2 border-gray-300 rounded-box w-40"
+          >
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/">Logout</Link>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
