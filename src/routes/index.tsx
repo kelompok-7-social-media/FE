53c9e4bfd2b8ffd776ea155d857d0abb5fd55ee9
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-17 12:13:04 +0700

    feat: implemented react routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
