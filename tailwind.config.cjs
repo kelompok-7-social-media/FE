ada971eccb91aad686cce90e98ff9da80a32584f
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-17 16:42:04 +0700

    chore: extended tailwindcss theme color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgpurple: "#5B0057",
+        bgpink: "#AB0052",
+        pink: "#A91079",
+      },
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
