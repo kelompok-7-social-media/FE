b08f0164904883e8805b83a76731d9293148577b
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-18 01:06:18 +0700

    feat: created timeline page with responsive design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { IoIosArrowDown } from "react-icons/io";
 
 const Navbar = () => {
   return (
-    <div className="navbar px-2 bg-gradient-to-b from-bgpurple to-bgpink md:px-5">
+    <div className="navbar px-2 bg-gradient-to-b from-bgpurple to-bgpink md:px-5 mb-1">
       <div className="navbar-start">
         <img className="w-12" src="src/assets/logo-white.svg" alt="logo" />
         <p className="normal-case text-white font-bold mx-1 md:hidden">
