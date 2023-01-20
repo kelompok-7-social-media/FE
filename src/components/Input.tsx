92d2a314a8863db7d433f467d7c35d1c883c95e7
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-20 07:20:24 +0700

    feat: added post input in input components

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -13,3 +13,13 @@ export function CustomInput({ id, ...props }: Props) {
     />
   );
 }
+
+export function PostInput({ id, ...props }: Props) {
+  return (
+    <input
+      id={id}
+      className="w-full rounded-2xl py-5 px-5 border-2 border-gray-500 mb-3"
+      {...props}
+    />
+  );
+}
