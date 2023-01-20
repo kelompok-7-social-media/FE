7842c9dba02fee6d4832008b9ad3d496e51b3468
Ahmad Zain AF <zainazharul@gmail.com>
2023-01-20 07:23:11 +0700

    feat: created post

--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,20 +1,26 @@
-import { BiImageAdd, BiDotsHorizontalRounded } from "react-icons/bi";
-import { useState, useEffect } from "react";
+import withReactContent from "sweetalert2-react-content";
+import React, { useState, useEffect } from "react";
+import { BiImageAdd } from "react-icons/bi";
+import { useNavigate } from "react-router";
 import { useCookies } from "react-cookie";
-import moment from "moment";
 import axios from "axios";
 
 import { ButtonWhite } from "components/Button";
+import PostCard from "components/PostCard";
 import Layout from "components/Layout";
 import Navbar from "components/Navbar";
-import PostCard from "components/PostCard";
 import { useTitle } from "utils/hooks/customHooks";
 import { PostType } from "utils/types/post";
+import Swal from "utils/Swal";
 
 const Timeline = () => {
   useTitle(`Galaxy - Timeline`);
   const [cookie] = useCookies(["token"]);
+  const MySwal = withReactContent(Swal);
+  const navigate = useNavigate();
   const [datas, setDatas] = useState<PostType[]>([]);
+  const [description, setDescription] = useState<string>("");
+  const [file_photos, setFilePhotos] = useState<[]>([]);
   const checkToken = cookie.token;
 
   useEffect(() => {
@@ -33,6 +39,34 @@ const Timeline = () => {
       });
   }
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const body = {
+      description,
+      file_photos,
+    };
+    axios
+      .post("posts", body)
+      .then((res) => {
+        const { data, message } = res.data;
+        MySwal.fire({
+          title: "Posted",
+          text: message,
+          showCancelButton: false,
+        });
+        navigate("timeline");
+      })
+      .catch((err) => {
+        const { data } = err.response;
+        MySwal.fire({
+          title: "Failed",
+          text: data.message,
+          showCancelButton: false,
+        });
+      });
+  };
+
   return (
     <Layout>
       <Navbar />
@@ -45,11 +79,11 @@ const Timeline = () => {
               placeholder="What's on your mind?"
             />
             <div className="flex justify-end items-center gap-3">
-              <span className="text-white">
+              <span className="duration-300 active:scale-90 hover:cursor-pointer text-white">
                 <BiImageAdd size={30} />
               </span>
               <span>
-                <ButtonWhite label="Publish" />
+                <ButtonWhite type="submit" id="btn-post" label="Publish" />
               </span>
             </div>
           </section>
